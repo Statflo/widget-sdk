c247856a4e454e856850dd6e1047ecba88187899
Alexei Darmin <alexei.darmin@gmail.com>
2022-03-02 15:34:09 -0500

    pages prototype working

---
+++ docs/main.js
@@ -0,0 +1,10 @@
+
+var ContainerClient = require('./dist/index').ContainerClient;
+var WidgetClient = require('./dist/index').WidgetClient;
+var Helpers = require('./dist/index').Helpers;
+
+window.Statflo = {
+  ContainerClient,
+  WidgetClient,
+  Helpers,
+}
